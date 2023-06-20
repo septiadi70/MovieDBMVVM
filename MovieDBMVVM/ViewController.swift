708762b99c8684d4cd3493f1517aabb310956816
septiadi70 <septiadi70@gmail.com>
2023-06-20 23:14:44 +0700

    Initial commit

---
+++ MovieDBMVVM/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MovieDBMVVM
+//
+//  Created by Andi Septiadi on 20/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
+
